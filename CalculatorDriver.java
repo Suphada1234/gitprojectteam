5cb346e74c4ef838481525c040b2b92032d69b4a
Suphada Thongthai <614259054@webmail.npru.ac.th>
2020-09-03 12:04:29 +0700

    Updata Use Calculator

--- CalculatorDriver.java
+++ CalculatorDriver.java
@@ -2,6 +2,6 @@ public class CalculatorDriver{
 	public static void main(String[] args){
 		Calculator c1 = new Calculator();
 		System.out.println(c1.add(5,4));
-
+		System.out.println(c1.multiply(5,4));
 	}
 }
